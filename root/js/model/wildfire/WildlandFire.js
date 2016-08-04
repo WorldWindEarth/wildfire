0a4262eb5064414baa2122c3615b673e2dc22cc2
Bruce Schubert <bruce@emxsys.com>
2016-08-04 06:58:28 -0700

    Updated WildlandFire paths and constants.

--- root/js/model/wildfire/WildlandFire.js
+++ root/js/model/wildfire/WildlandFire.js
@@ -6,23 +6,19 @@
 /*global define, WorldWind*/
 
 define([
-    'require',
-    'wmt/util/ContextSensitive',
-    'wmt/resource/GeoMacResource',
-    'wmt/util/Openable',
-    'wmt/util/Log',
-    'wmt/util/Messenger',
-    'wmt/util/WmtUtil',
-    'wmt/Wmt'],
+    'model/util/ContextSensitive',
+    'model/services/GeoMacService',
+    'model/util/Openable',
+    'model/util/Log',
+    'model/util/WmtUtil',
+    'model/Constants'],
     function (
-        require,
         contextSensitive,
         geoMac,
         openable,
         log,
-        messenger,
         util,
-        wmt) {
+        constants) {
         "use strict";
 
         var WildlandFire = function (feature) {
@@ -30,21 +26,19 @@ define([
 
             // Make openable via menus: Fires the EVENT_OBJECT_OPENED event on success.
             openable.makeOpenable(this, function () {
-                messenger.infoGrowl("The open feature has not been implemented yet.", "Sorry");
+                //messenger.infoGrowl("The open feature has not been implemented yet.", "Sorry");
                 return false;
             });
 
             // Make context sensiive by the SelectController: shows the context menu.
-            contextSensitive.makeContextSenstive(this, function () {
-                messenger.infoGrowl("Show menu with delete, open, and lock/unlock", "TODO");
+            contextSensitive.makeContextSensitive(this, function () {
+                //messenger.infoGrowl("Show menu with delete, open, and lock/unlock", "TODO");
             });
 
             /**
              * The unique id used to identify this particular object within WMTweb session. It is not persistant.
              */
             this.id = util.guid();
-
-            // First attempt to assign a "large fire point", the the "current fire perimeter" attribute
             this.name = attributes.incidentname
                 || attributes.fire_name
                 || 'Fire';
@@ -53,13 +47,13 @@ define([
                 || attributes.inc_num
                 || 'Unknown';
             this.featureId = attributes.objectid;
-            this.featureType = attributes.incidentname ? wmt.WILDLAND_FIRE_POINT : wmt.WILDLAND_FIRE_PERIMETER;
+            this.featureType = attributes.incidentname ? constants.WILDLAND_FIRE_POINT : constants.WILDLAND_FIRE_PERIMETER;
 
             // If the feature has geometry then process it, otherwise defer until needed
             if (feature.geometry) {
                 this.processGeometry(feature.geometry);
             } else {
-                this.geometryType = wmt.GEOMETRY_UNKNOWN;
+                this.geometryType = constants.GEOMETRY_UNKNOWN;
                 this.geometry = null;
                 this.extents = null;
             }
@@ -70,7 +64,7 @@ define([
          */
         WildlandFire.prototype.loadDeferredGeometry = function (deferred) {
             var self = this;
-            if (this.featureType === wmt.WILDLAND_FIRE_POINT) {
+            if (this.featureType === constants.WILDLAND_FIRE_POINT) {
                 geoMac.getActiveFireFeature(this.featureId,
                     function (feature) {
                         self.processGeometry(feature.geometry);
@@ -79,7 +73,7 @@ define([
                         }
                     });
             }
-            else if (this.featureType === wmt.WILDLAND_FIRE_PERIMETER) {
+            else if (this.featureType === constants.WILDLAND_FIRE_PERIMETER) {
                 geoMac.getActivePerimeterFeature(this.featureId,
                     function (feature) {
                         self.processGeometry(feature.geometry);
@@ -103,7 +97,7 @@ define([
             this.geometry = geometry;
 
             if (geometry.x && geometry.y) {
-                this.geometryType = wmt.GEOMETRY_POINT;
+                this.geometryType = constants.GEOMETRY_POINT;
 
                 // Set the "goto" locaiton
                 this.latitude = geometry.y;
@@ -111,7 +105,7 @@ define([
                 this.extents = null;
 
             } else if (geometry.rings) {
-                this.geometryType = wmt.GEOMETRY_POLYGON;
+                this.geometryType = constants.GEOMETRY_POLYGON;
 
                 // Compute the extents
                 minLat = Number.MAX_VALUE;
@@ -136,21 +130,21 @@ define([
         };
 
 
-        WildlandFire.prototype.goto = function () {
-            var controller = require("wmt/controller/Controller"),
-                deferred = $.Deferred();
-
-            if (this.geometry) {
-                controller.lookAtLatLon(this.latitude, this.longitude);
-            } else {
-                // Load the geometry
-                this.loadDeferredGeometry(deferred);
-                $.when(deferred).done(function (self) {
-                    controller.lookAtLatLon(self.latitude, self.longitude);
-                });
-
-            }
-        };
+//        WildlandFire.prototype.goto = function () {
+//            var controller = require("wmt/controller/Controller"),
+//                deferred = $.Deferred();
+//
+//            if (this.geometry) {
+//                controller.lookAtLatLon(this.latitude, this.longitude);
+//            } else {
+//                // Load the geometry
+//                this.loadDeferredGeometry(deferred);
+//                $.when(deferred).done(function (self) {
+//                    controller.lookAtLatLon(self.latitude, self.longitude);
+//                });
+//
+//            }
+//        };
 
 
         return WildlandFire;
