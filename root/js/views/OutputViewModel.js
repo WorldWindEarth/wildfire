138607841f813bd78c04a277df097a6d736658da
Bruce Schubert <bruce@emxsys.com>
2016-09-12 06:21:10 -0700

    Added Weather and Wildfire module dependencies. - Re: issue #3, expanded weather outputs. - Re: issue #8, expanded wildfire outputs.

--- root/js/views/OutputViewModel.js
+++ root/js/views/OutputViewModel.js
@@ -13,9 +13,14 @@
  */
 define(['knockout',
     'jquery',
+    'd3',
     'vis',
-    'model/Constants'],
-        function (ko, $, vis, constants) {
+    'model/Constants',
+    'views/WeatherOutputView',
+    'views/WildfireOutputView'],
+        function (ko, $, d3, vis, constants, 
+        WeatherOutputView,
+        WildfireOutputView) {
 
             /**
              * The view model for the Output panel.
@@ -24,13 +29,18 @@ define(['knockout',
             function OutputViewModel(globe) {
                 var self = this;
 
-                this.globe = globe;
+                this.globe = globe;                
+                
+                // Create the Knockout custom binding used in the #weather-scout-view-template
+                this.weatherView = new WeatherOutputView(globe);
+                // Create the Knockout custom binding used in the #weather-scout-view-template
+                this.wildfireView = new WildfireOutputView(globe);
 
+                // The viewTemplate defines the content displayed in the output pane
+                this.viewTemplateName = ko.observable(null);
+                
                 // Get a reference to the SelectController's selectedItem observable
                 this.selectedItem = this.globe.selectController.lastSelectedItem;
-
-                this.viewTemplateName = ko.observable(null);
-
                 this.selectedItem.subscribe(function (newItem) {
                     // Determine if the new item has a view template
                     if (newItem !== null) {
@@ -42,86 +52,88 @@ define(['knockout',
                     }
                 });
 
-                ko.bindingHandlers.visualizeWeather = {
-                    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-                        // This will be called when the binding is first applied to an element
-                        // Set up any initial state, event handlers, etc. here
-                    },
-                    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-                        // This will be called once when the binding is first applied to an element,
-                        // and again whenever any observables/computeds that are accessed change
-                        // Update the DOM element based on the supplied values here.
-
-                        // First get the latest data that we're bound to
-                        var value = valueAccessor();
-
-                        // Next, whether or not the supplied model property is observable, get its current value
-                        var valueUnwrapped = ko.unwrap(value);
-
-                        self.visualizeWeather(element, viewModel);
-                    }
-                };
+//                // Define the custom binding used in the #weather-scout-view-template template
+//                ko.bindingHandlers.visualizeWeather = {
+//                    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+//                        // This will be called when the binding is first applied to an element
+//                        // Set up any initial state, event handlers, etc. here
+//                    },
+//                    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+//                        // This will be called once when the binding is first applied to an element,
+//                        // and again whenever any observables/computeds that are accessed change
+//                        // Update the DOM element based on the supplied values here.
+//
+//                        // First get the latest data that we're bound to
+//                        var value = valueAccessor();
+//
+//                        // Next, whether or not the supplied model property is observable, get its current value
+//                        var valueUnwrapped = ko.unwrap(value);
+//
+//                        self.weatherView.visualizeWeather(element, viewModel);
+//                    }
+//                };
             }
 
-            OutputViewModel.prototype.visualizeWeather = function (element, wxScout) {
-                var forecasts = wxScout.getForecasts(),
-                        i, len, wx,
-                        items = [],
-                        names = ["F ", "RH %"],
-                        groups = new vis.DataSet();
-
-                groups.add({
-                    id: 0,
-                    content: names[0],
-                    options: {
-                        drawPoints: {
-                            style: 'square' // square, circle
-                        },
-                        shaded: {
-                            orientation: 'bottom' // top, bottom
-                        }
-                    }
-                });
-                groups.add({
-                    id: 1,
-                    content: names[1],
-                    options: {
-                        drawPoints: {
-                            style: 'circle' // square, circle
-                        },
-                        shaded: {
-                            orientation: 'top' // top, bottom
-                        },
-                        yAxisOrientation: 'right'
-                    }
-                });
-
-
-                for (i = 0, len = forecasts.length; i < len; i++) {
-                    wx = forecasts[i];
-                    items.push({x: wx.time, y: wx.airTemperatureF, group: 0, label: 'F'});
-                    items.push({x: wx.time, y: wx.relaltiveHumidityPct, group: 1});
-                }
-
-
-                var dataset = new vis.DataSet(items);
-                var options = {
-                    dataAxis: {
-                        left: {
-                            range: {min: 32, max: 120}
-                        },
-                        right: {
-                            range: {min: 0, max: 100}
-                        },
-                        icons: true
-                    },
-                    legend: true,
-                    height: 300,
-//                    start: forecasts[0].time,
-//                    end: forecasts[len-1].time
-                };
-                var graph2d = new vis.Graph2d(element, dataset, groups, options);
-            }
+//            
+//            OutputViewModel.prototype.visualizeWeather = function (element, wxScout) {
+//                var forecasts = wxScout.getForecasts(),
+//                        i, len, wx,
+//                        items = [],
+//                        names = ["F ", "RH %"],
+//                        groups = new vis.DataSet();
+//
+//                groups.add({
+//                    id: 0,
+//                    content: names[0],
+//                    options: {
+//                        drawPoints: {
+//                            style: 'square' // square, circle
+//                        },
+//                        shaded: {
+//                            orientation: 'bottom' // top, bottom
+//                        }
+//                    }
+//                });
+//                groups.add({
+//                    id: 1,
+//                    content: names[1],
+//                    options: {
+//                        drawPoints: {
+//                            style: 'circle' // square, circle
+//                        },
+//                        shaded: {
+//                            orientation: 'top' // top, bottom
+//                        },
+//                        yAxisOrientation: 'right'
+//                    }
+//                });
+//
+//
+//                for (i = 0, len = forecasts.length; i < len; i++) {
+//                    wx = forecasts[i];
+//                    items.push({x: wx.time, y: wx.airTemperatureF, group: 0, label: 'F'});
+//                    items.push({x: wx.time, y: wx.relaltiveHumidityPct, group: 1});
+//                }
+//
+//
+//                var dataset = new vis.DataSet(items);
+//                var options = {
+//                    dataAxis: {
+//                        left: {
+//                            range: {min: 32, max: 120}
+//                        },
+//                        right: {
+//                            range: {min: 0, max: 100}
+//                        },
+//                        icons: true
+//                    },
+//                    legend: true,
+//                    height: 300,
+////                    start: forecasts[0].time,
+////                    end: forecasts[len-1].time
+//                };
+//                var graph2d = new vis.Graph2d(element, dataset, groups, options);
+//            }
 
             return OutputViewModel;
         }
