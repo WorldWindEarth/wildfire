6409019248f41d61e8a7c60965f2d5bce8d482fd
Bruce Schubert <bruce@emxsys.com>
2016-09-23 04:48:56 -0700

    Haul chart work in progress.

--- root/js/views/FireLookoutView.js
+++ root/js/views/FireLookoutView.js
@@ -22,12 +22,20 @@ define(['knockout',
     'vis'],
         function (ko, $, d3, moment, vis) {
 
+
             /**
              * The view model for an individual FireLookout.
              * @constructor
              */
             function FireLookoutView() {
                 var self = this;
+                this.margin = {top: 20, right: 15, bottom: 60, left: 60};
+            
+                this.width = 400 - this.margin.left - this.margin.right;
+                this.height = 300 - this.margin.top - this.margin.bottom;
+                this.btuDomain = [10, 11000];
+                this.rosDomain = [1, 1100];
+                    
 
                 // Define custom Knockout bindings for a FireLookout.
                 ko.bindingHandlers.visualizeWildfireDiamond = {
@@ -45,7 +53,7 @@ define(['knockout',
                     init: function (element, valueAccessor, allBindings, lookout, bindingContext) {
                         // This will be called when the binding is first applied to an element
                         // Set up any initial state, event handlers, etc. here
-                        self.createHaulChart(element);                    
+                        self.createHaulChart(element);
                     },
                     update: function (element, valueAccessor, allBindings, lookout, bindingContext) {
                         // This will be called once when the binding is first applied to an element,
@@ -98,51 +106,61 @@ define(['knockout',
              * @param {type} element DOM element where the Timeline will be attached
              * @param {type} wildfire The Wildfire model element
              */
-            FireLookoutView.prototype.createHaulChart = function (element) {
-
-                var margin = {top: 20, right: 15, bottom: 60, left: 60}, 
-                        width = 400 - margin.left - margin.right, 
-                        height = 300 - margin.top - margin.bottom;
-
+            FireLookoutView.prototype.createHaulChart = function (element, lookout) {
+                var fireBehavior = lookout.fireBehavior;
+                var heatRelease = (fireBehavior === null ? 0 : fireBehavior.heatRelease),
+                    rateOfSpreadMax = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadMax),
+                    rateOfSpreadFlanking = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadFlanking),
+                    rateOfSpreadBacking = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadNoWindNoSlope),
+                    data = [[heatRelease, rateOfSpreadMax], [heatRelease, rateOfSpreadFlanking]];
+                
                 var x = d3.scaleLog() // btu
-                        .domain([10,11000])
-                        .range([0, width]);
+                        .domain(this.btuDomain)
+                        .range([0, this.width]);
 
                 var y = d3.scaleLog() // ros ft/min
-                        .domain([1, 1100])
-                        .range([height, 0]);
+                        .domain(this.rosDomain)
+                        .range([this.height, 0]);
 
                 var chart = d3.select(element)
                         .append('svg:svg')
-                        .attr('width', width + margin.right + margin.left)
-                        .attr('height', height + margin.top + margin.bottom)
+                        .attr('width', this.width + this.margin.right + this.margin.left)
+                        .attr('height', this.height + this.margin.top + this.margin.bottom)
                         .attr('class', 'chart')
 
                 var main = chart.append('g')
-                        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-                        .attr('width', width)
-                        .attr('height', height)
+                        .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
+                        .attr('width', this.width)
+                        .attr('height', this.height)
                         .attr('class', 'main')
 
                 // draw the x axis
                 var xAxis = d3.axisBottom(x);
-
                 main.append('g')
-                        .attr('transform', 'translate(0,' + height + ')')
-                        .attr('class', 'main axis date')
+                        .attr('transform', 'translate(0,' + this.height + ')')
+                        .attr('class', 'main axis btu')
                         .call(xAxis);
 
                 // draw the y axis
                 var yAxis = d3.axisLeft(y);
-
                 main.append('g')
                         .attr('transform', 'translate(0,0)')
-                        .attr('class', 'main axis date')
+                        .attr('class', 'main axis ros')
                         .call(yAxis);
 
                 var g = main.append("svg:g")
                         .attr('id', 'plot');
 
+                g.selectAll("scatter-dots")
+                        .data(data)
+                        .enter().append("svg:circle")
+                        .attr("cx", function (d) {
+                            return x(d[0]);
+                        })
+                        .attr("cy", function (d) {
+                            return y(d[1]);
+                        })
+                        .attr("r", 8);
             };
             /**
              * Display the percent contained in a radial progress chart via d3, 
@@ -153,27 +171,40 @@ define(['knockout',
              * @param {type} wildfire The Wildfire model element
              */
             FireLookoutView.prototype.updateHaulChart = function (element, lookout) {
-                lookout.fireBehavior.subscribe(function(fireBehavior) {
+                var self = this;
+                // Everytime the selected FireLookout is changed, we need to 
+                // subscribe to the lookout's fire behavior observable to update
+                // the chart when the fire behavior changes.
+                // 
+                // TODO: does the leak if we don't unsubscribe?
+                lookout.fireBehavior.subscribe(function (fireBehavior) {
+
+                    var heatRelease = (fireBehavior === null ? 0 : fireBehavior.heatRelease),
+                        rateOfSpreadMax = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadMax),
+                        rateOfSpreadFlanking = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadFlanking),
+                        rateOfSpreadBacking = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadNoWindNoSlope),
+                        data = [[heatRelease, rateOfSpreadMax], [heatRelease, rateOfSpreadFlanking]];
+
+                    var x = d3.scaleLog() // btu
+                        .domain(self.btuDomain)
+                        .range([0, self.width]);
+
+                    var y = d3.scaleLog() // ros ft/min
+                        .domain(self.rosDomain)
+                        .range([self.height, 0]);
                     
-                var heatRelease = (fireBehavior === null ? 0 : fireBehavior.heatRelease),
-                    rateOfSpreadMax = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadMax),
-                    rateOfSpreadFlanking = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadFlanking),
-                    rateOfSpreadBacking = (fireBehavior === null ? 0 : fireBehavior.rateOfSpreadNoWindNoSlope),
-                    data = [[heatRelease, rateOfSpreadMax], [heatRelease, rateOfSpreadFlanking]];
+                    var chart = d3.select(element);
+                    var g = chart.selectAll("#plot");
 
-                var chart = d3.select(element);
-                var g = chart.selectAll("#plot");
-
-                g.selectAll("scatter-dots")
+                    g.selectAll("scatter-dots")
                         .data(data)
-                        .enter().append("svg:circle")
-                            .attr("cx", function (d, i) {
-                                return x(d[0]);
-                            })
-                            .attr("cy", function (d) {
-                                return y(d[1]);
-                            })
-                            .attr("r", 8);
+                        .attr("cx", function (d) {
+                            return x(d[0]);
+                        })
+                        .attr("cy", function (d) {
+                            return y(d[1]);
+                        })
+                        .attr("r", 8);
                 });
             };
 
