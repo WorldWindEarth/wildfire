65ed5fd1b9dc620113fe0e84565904b0e518d40c
Bruce Schubert <bruce@emxsys.com>
2018-03-11 16:24:37 -0700

    Refactored FireLookoutManager to use self instead of this.

--- root/js/model/wildfire/FireLookoutManager.js
+++ root/js/model/wildfire/FireLookoutManager.js
@@ -9,7 +9,8 @@ define([
     'model/wildfire/FireLookout',
     'model/util/Log',
     'model/Constants',
-    'knockout'],
+    'knockout',
+    'worldwind'],
     function (
         FireLookout,
         log,
@@ -48,188 +49,193 @@ define([
 
             }, null, "arrayChange");
 
-        };
-
-
-        /**
-         * Adds the given lookout to to the manager.
-         * @param {FireLookout} lookout
-         */
-        FireLookoutManager.prototype.addLookout = function (lookout) {
-            // Place the lookout under the management of this module
-            this.lookouts.push(lookout);
-        };
-        /**
-         * Finds the fire lookout with the given id.
-         * @param {String} id System assigned id for the lookout.
-         * @returns {FireLookout} The lookout object if found, else null.
-         */
-        FireLookoutManager.prototype.findLookout = function (id) {
-            var lookout, i, len;
-            for (i = 0, len = this.lookouts().length; i < len; i += 1) {
-                lookout = this.lookouts()[i];
-                if (lookout.id === id) {
-                    return lookout;
-                }
-            }
-            return null;
-        };
-        /**
-         * Removes the given lookout from the manager.
-         * @param {FireLookout} lookout
-         * @returns {Boolean}
-         */
-        FireLookoutManager.prototype.removeLookout = function (lookout) {
-            this.lookouts.remove(lookout);
-        };
-
-        /**
-         * Invokes refresh on all the lookouts managed by this manager.
-         */
-        FireLookoutManager.prototype.refreshLookouts = function () {
-            var i, max;
-
-            for (i = 0, max = this.lookouts().length; i < max; i++) {
-                this.lookouts()[i].refresh();
-            }
-        };
-        // Internal method to ensure the name is unique by appending a suffix if reqd.
-        FireLookoutManager.prototype.doEnsureUniqueName = function (lookout) {
-            lookout.name(this.generateUniqueName(lookout));
-        };
-        // Internal method to add the lookout to the layer.
-        FireLookoutManager.prototype.doAddLookoutToLayer = function (lookout) {
-            this.layer.addRenderable(lookout.renderable);
-        };
-
-        // Internal method to remove the lookout's renderable from its layer.
-        FireLookoutManager.prototype.doRemoveLookoutFromLayer = function (lookout) {
-            var i, max, renderable = lookout.renderable;
-            // Remove the renderable from the renderable layer
-            for (i = 0, max = this.layer.renderables.length; i < max; i++) {
-                if (this.layer.renderables[i] === renderable) {
-                    this.layer.renderables.splice(i, 1);
-                    break;
-                }
-            }
-        };
-
-
-        /**
-         * Saves the fire lookouts collection to local storage.
-         */
-        FireLookoutManager.prototype.saveLookouts = function () {
-            var validLookouts = [],
-                lookoutsString,
-                i, len, lookout;
-
-            // Knockout's toJSON cannot process a WeatherScout/FireLookout object...
-            // it appears to recurse and a call stack limit is reached.
-            // So we create a simplfied the object here to pass to ko.toJSON()
-            for (i = 0, len = this.lookouts().length; i < len; i++) {
-                lookout = this.lookouts()[i];
-                if (!lookout.invalid) {
-                    validLookouts.push({
-                        id: lookout.id,
-                        name: lookout.name,
-                        latitude: lookout.latitude,
-                        longitude: lookout.longitude,
-                        isMovable: lookout.isMovable,
-                        fuelModelNo: lookout.fuelModelNo,
-                        fuelModelManualSelect: lookout.fuelModelManualSelect,
-                        moistureScenarioName: lookout.moistureScenarioName
-                    });
-                }
-            }
-            lookoutsString = ko.toJSON(validLookouts, [
-                'id',
-                'name',
-                'latitude',
-                'longitude',
-                'isMovable',
-                'fuelModelNo',
-                'fuelModelManualSelect',
-                'moistureScenarioName'
-            ]);
-            localStorage.setItem(constants.STORAGE_KEY_FIRE_LOOKOUTS, lookoutsString);
-        };
-
-        /**
-         * Restores the fire lookouts collection from local storage.
-         */
-        FireLookoutManager.prototype.restoreLookouts = function () {
-            var string = localStorage.getItem(constants.STORAGE_KEY_FIRE_LOOKOUTS),
-                array, i, max, item,
-                position, params;
-            if (!string || string === 'null') {
-                return;
-            }
-            // Convert JSON array to array of FireLookout objects
-            array = JSON.parse(string);
-            for (i = 0, max = array.length; i < max; i++) {
-                item = array[i];
-                position = new WorldWind.Position(item.latitude, item.longitude, 0);
-                params = {
-                    id: item.id,
-                    name: item.name,
-                    latitude: item.latitude,
-                    longitude: item.longitude,
-                    isMovable: item.isMovable,
-                    fuelModelNo: item.fuelModelNo,
-                    fuelModelManualSelect: item.fuelModelManualSelect,
-                    moistureScenarioName: item.moistureScenarioName
-                };
-                this.addLookout(new FireLookout(this, position, params));
-            }
-        };
-
-        /**
-         * Generates a unique name by appending a suffix '(n)'.
-         * @param {String} name
-         * @returns {String}
-         */
-        FireLookoutManager.prototype.generateUniqueName = function (lookout) {
-            var uniqueName = lookout.name().trim(),
-                otherLookout,
-                isUnique,
-                suffixes,
-                seqNos,
-                n, i, len;
-
-            // Loop while name not unique
-            do {
-                // Assume uniqueness, set to false if we find a matching name
-                isUnique = true;
-
-                // Test the name for uniqueness with the other scouts
-                for (i = 0, len = this.lookouts().length; i < len; i += 1) {
-                    otherLookout = this.lookouts()[i];
-                    if (otherLookout === lookout) {
-                        continue; // Don't test with self
+            /**
+             * Adds the given lookout to to the manager.
+             * @param {FireLookout} lookout
+             */
+            this.addLookout = function (lookout) {
+                // Place the lookout under the management of this module
+                self.lookouts.push(lookout);
+            };
+            
+            /**
+             * Centers the globe other the given lookout.
+             * @param {FireLookout} lookout
+             */
+            this.gotoLookout = function (lookout) {
+                self.globe.goto(lookout.latitude(), lookout.longitude());
+            };
+            /**
+             * Finds the fire lookout with the given id.
+             * @param {String} id System assigned id for the lookout.
+             * @returns {FireLookout} The lookout object if found, else null.
+             */
+            this.findLookout = function (id) {
+                var lookout, i, len;
+                for (i = 0, len = self.lookouts().length; i < len; i += 1) {
+                    lookout = self.lookouts()[i];
+                    if (lookout.id === id) {
+                        return lookout;
                     }
-                    if (otherLookout.name() === uniqueName) {
-                        isUnique = false;
+                }
+                return null;
+            };
 
-                        // check for existing suffix '(n)' and increment
-                        suffixes = uniqueName.match(/[(]\d+[)]$/);
-                        if (suffixes) {
-                            // increment an existing suffix's sequence number
-                            seqNos = suffixes[0].match(/\d+/);
-                            n = parseInt(seqNos[0], 10) + 1;
-                            uniqueName = uniqueName.replace(/[(]\d+[)]$/, '(' + n + ')');
-                        } else {
-                            // else if no suffix, create one
-                            uniqueName += ' (2)';   // The first duplicate is #2
-                        }
-                        // Break out of the for loop and recheck uniqueness
+            /**
+             * Removes the given lookout from the manager.
+             * @param {FireLookout} lookout
+             * @returns {Boolean}
+             */
+            this.removeLookout = function (lookout) {
+                self.lookouts.remove(lookout);
+            };
+
+            /**
+             * Invokes refresh on all the lookouts managed by this manager.
+             */
+            this.refreshLookouts = function () {
+                var i, max;
+
+                for (i = 0, max = self.lookouts().length; i < max; i++) {
+                    self.lookouts()[i].refresh();
+                }
+            };
+            // Internal method to ensure the name is unique by appending a suffix if reqd.
+            this.doEnsureUniqueName = function (lookout) {
+                lookout.name(self.generateUniqueName(lookout));
+            };
+            // Internal method to add the lookout to the layer.
+            this.doAddLookoutToLayer = function (lookout) {
+                self.layer.addRenderable(lookout.renderable);
+            };
+
+            // Internal method to remove the lookout's renderable from its layer.
+            this.doRemoveLookoutFromLayer = function (lookout) {
+                var i, max, renderable = lookout.renderable;
+                // Remove the renderable from the renderable layer
+                for (i = 0, max = self.layer.renderables.length; i < max; i++) {
+                    if (self.layer.renderables[i] === renderable) {
+                        self.layer.renderables.splice(i, 1);
                         break;
                     }
                 }
-            } while (!isUnique);
+            };
 
-            return uniqueName;
+            /**
+             * Saves the fire lookouts collection to local storage.
+             */
+            this.saveLookouts = function () {
+                var validLookouts = [],
+                    lookoutsString,
+                    i, len, lookout;
+
+                // Knockout's toJSON cannot process a WeatherScout/FireLookout object...
+                // it appears to recurse and a call stack limit is reached.
+                // So we create a simplfied the object here to pass to ko.toJSON()
+                for (i = 0, len = self.lookouts().length; i < len; i++) {
+                    lookout = self.lookouts()[i];
+                    if (!lookout.invalid) {
+                        validLookouts.push({
+                            id: lookout.id,
+                            name: lookout.name,
+                            latitude: lookout.latitude,
+                            longitude: lookout.longitude,
+                            isMovable: lookout.isMovable,
+                            fuelModelNo: lookout.fuelModelNo,
+                            fuelModelManualSelect: lookout.fuelModelManualSelect,
+                            moistureScenarioName: lookout.moistureScenarioName
+                        });
+                    }
+                }
+                lookoutsString = ko.toJSON(validLookouts, [
+                    'id',
+                    'name',
+                    'latitude',
+                    'longitude',
+                    'isMovable',
+                    'fuelModelNo',
+                    'fuelModelManualSelect',
+                    'moistureScenarioName'
+                ]);
+                localStorage.setItem(constants.STORAGE_KEY_FIRE_LOOKOUTS, lookoutsString);
+            };
+
+            /**
+             * Restores the fire lookouts collection from local storage.
+             */
+            this.restoreLookouts = function () {
+                var string = localStorage.getItem(constants.STORAGE_KEY_FIRE_LOOKOUTS),
+                    array, i, max, item,
+                    position, params;
+                if (!string || string === 'null') {
+                    return;
+                }
+                // Convert JSON array to array of FireLookout objects
+                array = JSON.parse(string);
+                for (i = 0, max = array.length; i < max; i++) {
+                    item = array[i];
+                    position = new WorldWind.Position(item.latitude, item.longitude, 0);
+                    params = {
+                        id: item.id,
+                        name: item.name,
+                        latitude: item.latitude,
+                        longitude: item.longitude,
+                        isMovable: item.isMovable,
+                        fuelModelNo: item.fuelModelNo,
+                        fuelModelManualSelect: item.fuelModelManualSelect,
+                        moistureScenarioName: item.moistureScenarioName
+                    };
+                    self.addLookout(new FireLookout(self, position, params));
+                }
+            };
+
+            /**
+             * Generates a unique name by appending a suffix '(n)'.
+             * @param {FireLookout} lookout
+             * @returns {String}
+             */
+            this.generateUniqueName = function (lookout) {
+                var uniqueName = lookout.name().trim(),
+                    otherLookout,
+                    isUnique,
+                    suffixes,
+                    seqNos,
+                    n, i, len;
+
+                // Loop while name not unique
+                do {
+                    // Assume uniqueness, set to false if we find a matching name
+                    isUnique = true;
+
+                    // Test the name for uniqueness with the other scouts
+                    for (i = 0, len = self.lookouts().length; i < len; i += 1) {
+                        otherLookout = self.lookouts()[i];
+                        if (otherLookout === lookout) {
+                            continue; // Don't test with self
+                        }
+                        if (otherLookout.name() === uniqueName) {
+                            isUnique = false;
+
+                            // check for existing suffix '(n)' and increment
+                            suffixes = uniqueName.match(/[(]\d+[)]$/);
+                            if (suffixes) {
+                                // increment an existing suffix's sequence number
+                                seqNos = suffixes[0].match(/\d+/);
+                                n = parseInt(seqNos[0], 10) + 1;
+                                uniqueName = uniqueName.replace(/[(]\d+[)]$/, '(' + n + ')');
+                            } else {
+                                // else if no suffix, create one
+                                uniqueName += ' (2)';   // The first duplicate is #2
+                            }
+                            // Break out of the for loop and recheck uniqueness
+                            break;
+                        }
+                    }
+                } while (!isUnique);
+
+                return uniqueName;
+            };
         };
-
         return FireLookoutManager;
     }
 );
