414475f91368ba45d13ff46e4a197e1c9cb43e9a
Bruce Schubert <bruce@emxsys.com>
2018-03-11 09:07:10 -0700

    Removed redundant block of code in LayerManager.js

--- root/js/model/globe/LayerManager.js
+++ root/js/model/globe/LayerManager.js
@@ -112,17 +112,7 @@ define([
             /**
              * A collection of servers added to the layer manager by the user.
              */
-            LayerManager.prototype.loadDefaultLayers = function () {
-                // Define the Globe's default layers
-                this.addBaseLayer(new WorldWind.BMNGLayer(), {enabled: true, hideInMenu: false, detailControl: config.imagerydetailControl});
-                this.addBaseLayer(new WorldWind.BMNGLandsatLayer(), {enabled: false, detailControl: config.imagerydetailControl});
-                this.addBaseLayer(new WorldWind.BingAerialWithLabelsLayer(null), {enabled: false, detailControl: config.imagerydetailControl});
-                this.addBaseLayer(new EoxSentinal2CloudlessLayer(), {enabled: false, detailControl: config.imagerydetailControl});
-                this.addBaseLayer(new EoxSentinal2WithLabelsLayer(), {enabled: false, detailControl: config.imagerydetailControl});
-                this.addBaseLayer(new UsgsImageryTopoBaseMapLayer(), {enabled: false, detailControl: 1.75});
-                this.addBaseLayer(new UsgsTopoBaseMapLayer(), {enabled: false, detailControl: config.imagerydetailControl});
-                this.addBaseLayer(new WorldWind.BingRoadsLayer(null), {enabled: false, opacity: 0.7, detailControl: config.imagerydetailControl});
-                this.addBaseLayer(new EoxOpenStreetMapLayer(), {enabled: false, opacity: 0.8, detailControl: config.imagerydetailControl});
+            this.servers = ko.observableArray();
 
             /**
              * An ordered list of the layer category arrays useful for iterating over all the layers. 
