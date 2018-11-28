8ac01d59ae102717fdffcfeb5282586df6561f2d
Bruce Schubert <bruce@emxsys.com>
2018-11-28 05:48:16 -0800

    Aligned fire behavior adjectives to colors in Fire Lookout callout
    - Re: #40

--- root/js/model/wildfire/symbols/FireBehaviorCallout.js
+++ root/js/model/wildfire/symbols/FireBehaviorCallout.js
@@ -54,9 +54,9 @@ define(['model/Constants', 'model/globe/EnhancedAnnotation', 'model/util/Formatt
                   
                   
                   this.text = "Potential Fire Behavior @ " + (dateTime.isValid() ? dateTime.format("llll") : "?") +
+                    "\nFuel Model: " + modelNo + " - " + modelName +
                     "\nWinds from " + windDir + "° at " + windSpd + " knots" +
                     "\nSlope aspect is " + aspect + " at " + slope +
-                    "\nFuel Model: " + modelNo + " - " + modelName +
                     "\nHead Fire: " + FireBehaviorCallout.formatFlameLength(head) +
                     "\nFlanking Fire: " + FireBehaviorCallout.formatFlameLength(flanks) +
                     "\nBacking Fire: " + FireBehaviorCallout.formatFlameLength(heel) +
@@ -71,8 +71,8 @@ define(['model/Constants', 'model/globe/EnhancedAnnotation', 'model/util/Formatt
                 if (Number.isNaN(flameLen)) {
                     return "NA"
                 } else {
-                    var len = Number(flameLen).toFixed(flameLen > 1 ? 0 : 1);
-                    return len + "' - " + (len < 1 ? "LOW" : len < 3 ? "MODERATE" : len < 7 ? "ACTIVE" : len < 15 ? "VERY ACTIVE" : "EXTREME");
+                    var len = Number(flameLen).toFixed(flameLen > 3 ? 0 : 1);
+                    return len + "' - " + (flameLen <= 1 ? "LOW" : flameLen <= 3 ? "MODERATE" : flameLen <= 7 ? "ACTIVE" : flameLen <= 15 ? "VERY ACTIVE" : "EXTREME");
                 }
             };
             
